d78de852219d71f68209fad6c0d6487b2f8b981a
игорь <gasper.tan@yandex.ru>
2023-10-09 11:36:35 +0300

    task6

---
+++ src/task6.js
@@ -0,0 +1,6 @@
+const fuelConsumption = 7.5; 
+const distance = 200; 
+const fuelPrice = 2.5; 
+const cost = (fuelConsumption * distance * fuelPrice);
+
+console.log(`Стоимость поездки составит: ${cost} рублей.`);
